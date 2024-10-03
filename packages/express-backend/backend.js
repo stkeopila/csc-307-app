433d89c2a3c5f8054063ef8e63263f0c2713882f
stkeopila <skeopila@calpoly.edu>
2024-10-02 19:06:57 -0700

    finished part 5 of lab

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,12 +36,26 @@ const users = {
 
 app.use(express.json());
 
+const findUserById = (id) => 
+  users["users_list"].find((user) => user["id"] === id);
+
 
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
   );
 };
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found haha.");
+  } 
+  else {
+    res.send(result);
+  }
+});
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
