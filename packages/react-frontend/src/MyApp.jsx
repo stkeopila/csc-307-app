d1351b0d761c77e504e238d75f409cd2e725e1c9
stkeopila <skeopila@calpoly.edu>
2024-10-09 23:34:32 -0700

    Finished IA part 3 submit and post calls

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -16,10 +16,26 @@ function MyApp() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
   }
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+}
   
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
   useEffect(() => {
     fetchUsers ()
     .then((res) => res.json())
