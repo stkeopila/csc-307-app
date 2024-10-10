61a6aeaab2e451cadf511e3f479c5867b98ffe1f
stkeopila <skeopila@calpoly.edu>
2024-10-09 20:15:18 -0700

    finished 1 of ia3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,19 +1,31 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, {useState, useEffect} from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
     setCharacters(updated);
   }
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
   function updateList(person) {
     setCharacters([...characters, person]);
   }
+  
+  useEffect(() => {
+    fetchUsers ()
+    .then((res) => res.json())
+    .then((json) => setCharacters(json["users_list"]))
+    .catch((error) => { console.log(error); });
+  }, [] );
 
   return (
     <div className="container">
