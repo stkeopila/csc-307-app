fbc7b6142348ccdea487ca68751fd10675935a4d
stkeopila <skeopila@calpoly.edu>
2024-10-09 22:25:06 -0700

    Can run npm run dev in root and finished IA 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -44,6 +45,7 @@ const users = {
   ]
 };
 
+app.use(cors());
 app.use(express.json());
 
 const findUserById = (id) => 
